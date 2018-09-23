606411bfbd79a0f359b7149572e2660f5318c282
原田 拓磨 <t.harada@april-knights.com>
2018-09-24 00:36:48 +0900

    drtest start up

---
+++ drtest/js/index.js
@@ -0,0 +1,19 @@
+$(function(){
+	$(".btn-gnavi").on("click", function(){
+		// ハンバーガーメニューの位置を設定するための変数
+		var rightVal = 0;
+		if($("#navi").hasClass("open")) {
+			// 「open」クラスを持つ要素はメニューを開いた状態に設定
+			// メニューを開いたら次回クリック時は閉じた状態になるよう設定
+			$("#navi").removeClass("open").addClass("close");
+		} else {
+			// 「open」クラスを持たない要素はメニューを閉じた状態に設定 (rightVal は0の状態 )
+			// メニューを開いたら次回クリック時は閉じた状態になるよう設定
+			$("#navi").addClass("open").removeClass("close");
+		}
+
+		$("#global-navi").stop().animate({
+			right: rightVal
+		}, 200);
+	});
+});
